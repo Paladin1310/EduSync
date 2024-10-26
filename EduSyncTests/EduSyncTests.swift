41344cf69c517634538a27668cee20940286bcc2
Alexandr <sashazobnin1310121@gmail.com>
2024-10-26 23:12:19 +0300

    Initial Commit

---
+++ EduSyncTests/EduSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  EduSyncTests.swift
+//  EduSyncTests
+//
+//  Created by Александр on 10/26/24.
+//
+
+import Testing
+@testable import EduSync
+
+struct EduSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
